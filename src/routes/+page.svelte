cd3801bfa8e0453728ecdf1d713ed0af9a4f7ec2
Amaan Ullah <theamaan13@gmail.com>
2023-10-04 22:42:26 +0530

    new change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
+<script>
+  import Main from "../components/Main.svelte";
+</script>
 
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
-</style>
+<Main />
